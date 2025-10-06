2e821bca1028da92c4cd7b165de22d271e4ad054
Marco-Poelsma <marco.poelsma@gracia.lasalle.cat>
2025-10-06 18:46:44 +0200

    Add project files.

---
+++ S01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Specialized;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Thread nouThread = new Thread(EscriureA);
+        nouThread.Start();
+
+        Thread T1 = new Thread(EscriureB);
+        Thread T2 = new Thread(EscriureC);
+        T1.Start();
+        T2.Start();
+
+        Console.WriteLine("Tasca 2");
+    }
+
+    public static void EscriureA()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("A");
+            Thread.Sleep(10);
+        }
+    }
+
+    public static void EscriureB()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("B");
+            Thread.Sleep(5);
+        }
+    }
+
+    public static void EscriureC()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("C");
+            Thread.Sleep(20);
+        }
+    }
+}
